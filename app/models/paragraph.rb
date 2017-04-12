396aef7cf239d33a48623ffed43a027a1c706303
Tomás de Lima <tomasdelima@gmail.com>
2017-04-12 19:06:09 -0300

    Seeds for Posts and Paragraphs

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,3 +1,5 @@
 class Paragraph < ActiveRecord::Base
   belongs_to :post
+
+  STYLES = [:left, :center, :right, :quote, :indent1, :indent2, :indent3, :indent4]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  STDOUT.puts "WARNING: This operation will erase ALL Posts and Paragraphs. Type 'OK' to continue:"
+  input = STDIN.gets.strip
+  if input == 'OK'
+    paragraphs_count = Paragraph.count
+    posts_count = Post.count
+
+    Paragraph.destroy_all
+    Post.destroy_all
+
+    STDOUT.puts "Destroyed #{posts_count} posts and #{paragraphs_count} paragraphs"
+
+    (1..100).map do |i|
+      p = Post.create(title: "Test Post #{i}", status: 'published', author: User.all.sample, created_by: User.all.sample)
+      (1..10).map do |j|
+        Paragraph.create(body: "Paragraph #{11 - j}: Etiam posuere quam ac quam. Maecenas aliquet accumsan leo. Nullam dapibus fermentum ipsum. Etiam quis quam. Integer lacinia. Nulla est. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer vulputate sem a nibh rutrum consequat. Maecenas lorem. Pellentesque pretium", post: p, style: Paragraph::STYLES.sample)
+      end
+    end
+    STDOUT.puts "Created #{Post.count} posts and #{Paragraph.count} paragraphs"
+  else
+    STDOUT.puts "Skipped"
+  end
+end
