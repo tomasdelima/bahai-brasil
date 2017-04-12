e2cf728919b13d355dba749ffadb0c96f0e152bb
Tomás de Lima <tomasdelima@gmail.com>
2017-04-12 18:03:24 -0300

    Adds a simlpe DB module

---
+++ mobile/js/db.js
@@ -0,0 +1,33 @@
+import { AsyncStorage } from 'react-native'
+
+const DB = {
+  select: (table, where, orderBy) => {
+    return AsyncStorage.getItem(table).then((r) => {
+      var result = JSON.parse(r)
+      console.log('SELECT    : ' + table + ' ' + JSON.stringify(where) + ' => ' + result.length + ' records')
+      return result.filter((item) => {
+        var rr = true
+        for (var key in where) { rr = rr && where[key].indexOf(item[key]) >= 0 }
+        return rr
+      })
+    })
+  },
+  get: (key) => {
+    return AsyncStorage.getItem(key)
+  },
+  update: (table, objs) => {
+    console.log('UPDATE    : ' + objs.length + ' records')
+
+    return DB.select(table).then((result) => {
+      var objResult = {}
+      result.map((item) => objResult[item.id] = item)
+      objs.map((obj) => objResult[obj.id] = obj)
+      result = []
+      for (var key in objResult) { result.push(objResult[key]) }
+      AsyncStorage.setItem(table, JSON.stringify(result))
+      return result
+    })
+  },
+}
+
+module.exports = DB
--- mobile/js/index.js
+++ mobile/js/index.js
@@ -9,24 +9,21 @@ import {
 
 const Post = require('./post')
 const NavBar = require('./navbar')
+const DB = require('./db')
 const s = require('./styles')
 
 module.exports = React.createClass({
   componentDidMount() {
-    AsyncStorage.getItem('posts').then((posts) => {
-      // this.setState({posts: posts})
-      AsyncStorage.getItem('updated_at').then((updated_at) => {
+    DB.select('posts', {status: ['published']}).then((oldPosts) => {
+      this.setState({posts: oldPosts})
+      DB.get('updated_at').then((updated_at) => {
         var url = 'https://bahai-brasil.herokuapp.com/api/v1/posts.json?updated_at=' + ('2000-01-01')
-        console.log('FETCH:      ' + url)
+        console.log('FETCH     : ' + url)
         fetch(url).then((response) => {
           var parsedResponse = JSON.parse(response._bodyInit)
           var newPosts = JSON.parse(parsedResponse.data)
-          var allPosts = this.state.posts.concat(newPosts)
-
-          console.log('FETCHED:    ' + newPosts.length + ' posts')
-          this.setState({posts: allPosts})
-          AsyncStorage.setItem('posts', parsedResponse.data).then(() => {
-            AsyncStorage.setItem('updated_at', parsedResponse.time)
+          DB.update('posts', newPosts).then((updatedPosts) => {
+            this.setState({posts: updatedPosts})
           })
         })
       })
