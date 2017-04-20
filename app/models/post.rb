302493e2382da63ed2fa02d4e2cd3f5d36e15f0d
Tomás de Lima <tomasdelima@gmail.com>
2017-04-20 02:10:43 -0300

    Destroy dependent paragraphs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :created_by, class_name: "User"
-  has_many :paragraphs
+  has_many :paragraphs, dependent: :destroy
   accepts_nested_attributes_for :paragraphs
 
   validates :title, :status, :author, :category, :created_by, presence: true
