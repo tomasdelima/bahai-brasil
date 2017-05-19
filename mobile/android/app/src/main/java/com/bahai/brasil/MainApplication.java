47e3945f8a9c007565a122d4d0f43a674ff761ee
Tomás de Lima <tomasdelima@gmail.com>
2017-05-19 10:46:06 -0300

    Allows MultiDex

--- mobile/android/app/src/main/java/com/bahai/brasil/MainApplication.java
+++ mobile/android/app/src/main/java/com/bahai/brasil/MainApplication.java
@@ -1,6 +1,7 @@
 package com.bahai.brasil;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -14,7 +15,7 @@ import java.util.List;
 
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube; // CUSTOM
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
