41c9161e2096c1c6c9224a93bfab98fc7735f49e
Tomás de Lima <tomasdelima@gmail.com>
2017-09-17 13:07:44 -0300

    Adding Redux and React Redux

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
 
-class BahaiBrasil extends React.Component {
-  render () {
-    return <div>Hello {this.props.name}!</div>
-  }
-}
+global.store = createStore(todoApp)
+global.unsubscribe = store.subscribe(() => {})
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<BahaiBrasil />, document.getElementById('react-root'))
-})
+render(
+  <Provider store={store}>
+    <div>
+    </div>
+  </Provider>,
+  document.getElementById('react-root')
+)
+
+Object.merge = (obj1, obj2) => {obj1 = obj1 || {}; obj2 = obj2 || {};var obj3 = JSON.parse(JSON.stringify(obj1)); Object.keys(obj2).map((key) => obj3[key] = obj2[key]); return obj3}
---
+++ app/javascript/packs/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+
+const reducer = (state={}) => state
+
+export default combineReducers({reducer})
