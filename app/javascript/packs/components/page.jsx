e53d8660c1953e8b926433cd9706237e9ac20f94
Tomás de Lima <tomasdelima@gmail.com>
2017-09-19 19:03:51 -0300

    Adding Markdown

--- app/javascript/packs/components/page.jsx
+++ app/javascript/packs/components/page.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import MarkdownRenderer from 'react-markdown'
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      ...pages.filter((p) => p.slug == (this.props.match.params.slug))[0] || pages.filter((p) => p.slug == "")[0]
+    }
+  },
+  updateBody (e) {
+    this.setState({body: e.nativeEvent.target.value})
+  },
+  renderBody () {
+    if (window.location.hash == "#editor") {
+      return <textarea onChange={this.updateBody} value={this.state.body} style={[s.wide(), s.high(400)].merge()}/>
+    } else {
+      return <MarkdownRenderer source={this.state.body}/>
+    }
+  },
   render () {
-    var page = pages.filter((p) => p.slug == (this.props.match.params.slug))[0] || pages.filter((p) => p.slug == "")[0]
-
     return <div>
-      <div>
-        {page.body}
-      </div>
+      {this.renderBody()}
     </div>
   }
 })
