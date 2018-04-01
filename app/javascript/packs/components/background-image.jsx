90ee63ed095a9727a796e16a9c3e9eb2d9961043
Tomás de Lima <tomasdelima@gmail.com>
2018-04-01 02:41:13 -0400

    Removing old components

--- app/javascript/packs/components/background-image.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class BackgroundImage extends React.Component {
-  render () {
-    return this.props.image ? <div style={this.props.wide ? s.wide() : null, s.bgImage(images[this.props.image])}>
-      {this.props.children}
-    </div> : null
-  }
-}
