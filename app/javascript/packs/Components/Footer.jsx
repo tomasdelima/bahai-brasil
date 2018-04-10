ecb215eeea278a3957ee5e4e4441394977a589d6
Tomás de Lima <tomasdelima@gmail.com>
2018-04-09 23:18:00 -0400

    Footer responsive

--- app/javascript/packs/Components/Footer.jsx
+++ app/javascript/packs/Components/Footer.jsx
@@ -3,41 +3,43 @@ import Optimized from '../Lib/Optimized'
 
 export default class Footer extends Optimized {
   render () {
-    var style = [s.noDecoration, s.size(19), {fontFamily: 'Roboto', color: t.white}].merge()
+    var m = s.isMobile()
+    var fontSize = m ? "xx-large" : "larger"
+    var style = [s.noDecoration, s.size(fontSize), s.text, s.color(t.white)].merge()
 
-    return <Flex spacedOut BG={t.darkGreen} wide style={[s.padding(43, 0), style]}>
-      <Flex column>
-        <img style={s.wide(175)} src={images.logo}/>
+    return <Flex wrap spacedOut BG={t.darkGreen} wide style={[s.padding(43, 0), style]}>
+      <Flex column style={m && s.wide("50%")}>
+        <img style={s.wide(m ? "80%" : 175)} src={images.logo}/>
         <Flex style={s.margin(15, 0, 0)}>Copyright ©{new Date().getFullYear()}</Flex>
       </Flex>
 
-      <Flex stretch2 column>
-        <div>info@bahai.org.br</div>
-        <div>(+55 61) 3255 2200</div>
+      <Flex stretch2 column style={m && s.wide("50%")}>
+        <Flex>info@bahai.org.br</Flex>
+        <Flex>(+55 61) 3255 2200</Flex>
 
-        <Flex spacedOut size={32} style={s.margin(15, 0, 0)}>
+        <Flex spacedOut size={m ? 100 : 32} style={s.margin(15, 0, 0)}>
           <div className="fa fa-facebook-square"/>
           <div className="fa fa-youtube"/>
         </Flex>
       </Flex>
 
-      <Flex column start2 lineHeight="27px">
+      {!m && <Flex column start2 lineHeight="27px">
         <Link to={"/foo"} style={style}>Bahá'ís no Brasil</Link>
         <Link to={"/vida-espiritual"} style={style}>Vida Espiritual</Link>
         <Link to={"/foo"} style={style}>Notícias</Link>
         <Link to={"/foo"} style={style}>Orações</Link>
         <Link to={"/foo"} style={style}>Imprensa</Link>
-      </Flex>
+      </Flex>}
 
-      <Flex start2 column>
-        <div>Receba nossa newsletter</div>
+      <Flex start2 column style={m && [s.wide("100%"), s.center2, s.margin(50, 0, 0)]}>
+        <Flex>Receba nossa newsletter</Flex>
 
         <Flex style={s.margin(15, 0, 0)}>
           <style>
             #newsletter-email::placeholder {JSON.stringify(style).replace(/,/g,';').replace(/"/g,'')}
           </style>
 
-          <input id="newsletter-email" placeholder="email" style={[style, s.padding(12, 20), s.noBorder, s.radius("4px 0 0 4px"), s.BG(t.gray)].merge()}/>
+          <input id="newsletter-email" placeholder="email" type="email" style={[style, s.padding(12, 20), s.noBorder, s.radius("4px 0 0 4px"), s.BG(t.gray)].merge()}/>
           <Button to="foo" label="inscrever-se" margin="0" bgColor={t.green} radius="0 4px 4px 0"/>
         </Flex>
       </Flex>
