c3ff2909ff6e3173597a7e54c6b0cb692be51cbe
Tomás de Lima <tomasdelima@gmail.com>
2018-04-23 12:17:18 -0400

    FIX

--- db/migrate/20180423145159_add_parent_page_to_pages.rb
+++ db/migrate/20180423145159_add_parent_page_to_pages.rb
@@ -1,5 +1,6 @@
 class AddParentPageToPages < ActiveRecord::Migration[5.1]
   def change
-    add_reference :pages, :parent_page, foreign_key: true
+    # add_reference :pages, :parent_page, foreign_key: true
+    add_column :pages, :parent_page_id, :integer
   end
 end
